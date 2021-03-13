7f99f046bda9e10626e99a2499fa4a074eb339b3
aurora-madness <jakersaf@gmail.com>
2021-03-13 12:32:42 -0300

    Dando os primeiros passos com o projeto!

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,30 @@
-from soma import soma
+from basics import soma, multi
+lista = ["[0] soma", "[1] multiplicação"]
+funcs = [soma, multi]
+# listas com todas funções de cálculo
 
-# while True:
+print("Bem vindo ao Calculushub!")
 
-soma()
+while True:
+    print("Suas opções de contas são essas:")
+    for p in range(0, len(lista)):
+        print(lista[p])
+    escolha = int(input(f"Deseja fazer qual cálculo? "))
+    if escolha >= 2:
+        while True:
+            print("Opa! Esse número está fora do meu alcance!\nTente de novo, por favor")
+            print("Suas opções de contas são essas:")
+            for p in range(0, len(lista)):
+                print(lista[p])
+            escolha = int(input(f"Deseja fazer qual cálculo? "))
+            if escolha in range(0, len(lista)):
+                break
+    funcs[escolha]()
+    while True:
+        cont = input("Deseja continuar? [S/n]").strip().upper()
+        if cont in 'SN':
+            break
+            print("Opção inválida! Tente novamente")
+    if cont == 'N':
+        print("<<< Volte sempre! >>>")
+        break
---
+++ src/basics.py
@@ -0,0 +1,20 @@
+# lembrar de substituir tudo por loops
+def soma():
+    print("="*5, " SOMA FODA ", "="*5)
+
+    v1 = int(input("primeiro valor: "))
+    v2 = int(input("segundo valor: "))
+
+    s = v1 + v2
+
+    print(f"O valor da soma entre {v1} e {v2} eh igual a: {s}")
+
+def multi():
+    print("="*5, " MULTIPLICAÇÃO FODA ", "="*5)
+
+    m1 = int(input("primeiro valor: "))
+    m2 = int(input("segundo valor: "))
+
+    r = m1 * m2
+
+    print(f"O valor da multiplicação entre {m1} e {m2} eh igual a {r}")
